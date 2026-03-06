272922e9263654bf8d20c1351e1ebe323ec3684d
ksy <tndud4615@gmail.com>
2026-03-06 10:50:12 +0900

    init

---
+++ src/main/java/com/green/greenuni/GreenUniApplication.java
@@ -0,0 +1,13 @@
+package com.green.greenuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenUniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenUniApplication.class, args);
+    }
+
+}
