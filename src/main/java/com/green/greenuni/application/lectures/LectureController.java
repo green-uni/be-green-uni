f09295d6144b58ecb108309d4b060a58818199bd
chanmi <chanmi24@gmail.com>
2026-03-09 10:49:48 +0900

    lectures폴더 내 애노테이션 추가

---
+++ src/main/java/com/green/greenuni/application/lectures/LectureController.java
@@ -0,0 +1,13 @@
+package com.green.greenuni.application.lectures;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/lectures")
+@RequiredArgsConstructor
+
+public class LectureController {
+}
--- src/main/java/com/green/greenuni/application/lectures/lectureMapper.java
+++
@@ -1,4 +0,0 @@
-package com.green.greenuni.application.lectures;
-
-public class lectureMapper {
-}
