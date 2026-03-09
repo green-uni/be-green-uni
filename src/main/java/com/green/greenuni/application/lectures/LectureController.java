1d154b9cf72fc162e49512228be2b0fe056d7a62
chanmi <chanmi24@gmail.com>
2026-03-09 18:11:44 +0900

    LectureController 작성중

--- src/main/java/com/green/greenuni/application/lectures/LectureController.java
+++ src/main/java/com/green/greenuni/application/lectures/LectureController.java
@@ -1,7 +1,10 @@
 package com.green.greenuni.application.lectures;
 
 
+import com.green.greenuni.application.lectures.model.LectureCreateReq;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 
 public class LectureController {
+    private final LectureService lectureService;
+
+    @PostMapping("/create")
+    public ResultResponse<?> postLecture(@RequestBody LectureCreateReq req){
+        
+    }
+
+
 }
