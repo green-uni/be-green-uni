2f16ab729c5fb8ef9abc11f3034c806fbe861b57
ksy <tndud4615@gmail.com>
2026-03-11 09:58:58 +0900

    멤버,학과 생성 시 필요한 전공 목록 파일명 변경

--- src/main/java/com/green/greenuni/application/major/MajorMapper.java
+++ src/main/java/com/green/greenuni/application/major/MajorMapper.java
@@ -2,14 +2,14 @@ package com.green.greenuni.application.major;
 
 import com.green.greenuni.application.major.model.MajorCreateReq;
 import com.green.greenuni.application.major.model.MajorCreateRes;
-import com.green.greenuni.application.major.model.MajorListRes;
+import com.green.greenuni.application.major.model.MajorListForCreateRes;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface MajorMapper {
-    List<MajorListRes> findMajorAll();
+    List<MajorListForCreateRes> listForCreate();
     int createMajor(MajorCreateReq req); //학과 등록
     List<MajorCreateRes> getMajorList();
 }
