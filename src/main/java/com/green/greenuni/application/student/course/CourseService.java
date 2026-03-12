90ddad167ccb43ee6fcea5c4aa1a3e0723cb0841
junlee <myyouthhada@naver.com>
2026-03-12 18:10:45 +0900

    내 수강 목록 작업중

--- src/main/java/com/green/greenuni/application/student/course/CourseService.java
+++ src/main/java/com/green/greenuni/application/student/course/CourseService.java
@@ -1,6 +1,8 @@
 package com.green.greenuni.application.student.course;
 
 import com.green.greenuni.application.student.course.model.CourseListRes;
+import com.green.greenuni.application.student.course.model.MyCourseListRes;
+import com.green.greenuni.application.student.course.model.MyCourseResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,5 +17,17 @@ public class CourseService {
 
     public List<CourseListRes> getCourseList(){
         return courseMapper.getCourseList();
+    }
+
+    public MyCourseResponseDto getMyCourseData(long memberId) {
+        int totalCredits = courseMapper.getTotalEnrolledCredits(memberId);
+        List<MyCourseListRes> courseList = courseMapper.getMyCourseList(memberId);
+
+        MyCourseResponseDto response = new MyCourseResponseDto();
+        response.setMemberId(memberId);
+        response.setTotalEnrolledCredits(totalCredits);
+        response.setCourses(courseList);
+
+        return response;
     }
 }
---
+++ src/main/java/com/green/greenuni/application/student/course/model/MyCourseResponseDto.java
@@ -0,0 +1,16 @@
+package com.green.greenuni.application.student.course.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public class MyCourseResponseDto {
+    private long memberId;
+    private int totalEnrolledCredits;
+    private List<MyCourseListRes> courses;
+}
