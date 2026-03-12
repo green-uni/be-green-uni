c3ed06f5cb0c773c842fcf2afd4ecd08c221f727
chanmi <chanmi24@gmail.com>
2026-03-12 12:24:56 +0900

    전체강의목록작업중

---
+++ src/main/java/com/green/greenuni/application/lectures/model/LectureListRes.java
@@ -0,0 +1,26 @@
+package com.green.greenuni.application.lectures.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
+public class LectureListRes {
+    private String lectureType;
+    private String lectureName;
+    private String majorName;
+    private String proName;
+    private Integer credit;
+    private String day_of_week;
+    private String start_period;
+    private String end_period;
+    private String building;
+    private String roomNumber;
+    private Integer academicYear;
+    private Integer year;
+    private Integer semester;
+
+}
