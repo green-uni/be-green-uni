392bf28f7e0df52dc535d3ba69554cc43cfbafb6
junlee <myyouthhada@naver.com>
2026-03-13 14:22:14 +0900

    수강신청 및 취소 작업 끝

--- src/main/java/com/green/greenuni/application/course/CourseService.java
+++ src/main/java/com/green/greenuni/application/course/CourseService.java
@@ -2,7 +2,6 @@ package com.green.greenuni.application.course;
 
 import com.green.greenuni.application.course.model.*;
 import com.green.greenuni.application.lectures.model.LectureDetailRes;
-import com.green.greenuni.application.lectures.model.LectureStudentInfoReq;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
