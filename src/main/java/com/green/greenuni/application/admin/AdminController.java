2d02dde193fe0d1ae425730ebdd6d8219b7ec642
ksy <tndud4615@gmail.com>
2026-03-18 12:22:57 +0900

    계정 비밀번호 관련 작업

--- src/main/java/com/green/greenuni/application/admin/AdminController.java
+++ src/main/java/com/green/greenuni/application/admin/AdminController.java
@@ -25,7 +25,7 @@ public class AdminController {
             , @RequestPart(required = false) MultipartFile pic){
         log.info("createMember req: {}", req);
         MemberCreateRes res = memberService.createMember(req, pic);
-        return new ResultResponse("계정생성", res);
+        return new ResultResponse("계정 생성을 성공하였습니다", res);
     }
 
     // 전체 계정 조회
